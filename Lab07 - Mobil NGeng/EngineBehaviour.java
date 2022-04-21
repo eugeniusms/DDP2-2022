c560f74c54ec1782c4b4794dfc2b11078928f33c
eugeniusms <eugeniusms@gmail.com>
2022-04-21 14:51:32 +0700

    Interface Comments

--- Lab07 - Mobil NGeng/EngineBehaviour.java
+++ Lab07 - Mobil NGeng/EngineBehaviour.java
@@ -1,4 +1,10 @@
+// Interface sebuah mesin untuk digunakan dan diimplementasikan
 interface EngineBehaviour {
+    // Terdapat 3 behaviour
+    // 1. start -> Memulai penyalaan mesin
+    // 2. gas -> Menjalankan mesin
+    // 3. stop -> Memberhentikan mesin
+    
     public String start(Mobil mobil);
     public int gas(int persenFuel);
     public String stop(Mobil mobil);
--- Lab07 - Mobil NGeng/Listrik.java
+++ Lab07 - Mobil NGeng/Listrik.java
@@ -1,13 +1,17 @@
-public class Listrik implements EngineBehaviour { // TODO: implementasikan sesuai UML diagram
-    // TODO: tambahkan method-method yang diperlukan
+// Listrik implements dari EngineBehaviour Interface
+public class Listrik implements EngineBehaviour { 
+
+    // Default start method untuk mobil dengan daya listrik
     public String start(Mobil mobil) {
         return String.format("%s menyalakan listrik, SIAP DIGAS!", mobil.getNama());
     }
 
+    // Default gas method untuk mobil dengan daya listrik
     public int gas(int persenFuel) {
         return persenFuel-20;
     }
 
+    // Default stop method untuk mobil dengan daya listrik
     public String stop(Mobil mobil) {
         return String.format("%s listrik dimatikan, mobil telah berhenti.", mobil.getNama());
     }
