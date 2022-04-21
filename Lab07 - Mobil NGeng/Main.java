9f26be4c7f8ccba41b235d529fea78202e9fb9f6
eugeniusms <eugeniusms@gmail.com>
2022-04-21 15:06:22 +0700

    Main Comments

--- Lab07 - Mobil NGeng/Main.java
+++ Lab07 - Mobil NGeng/Main.java
@@ -1,21 +1,30 @@
+// Mengimport java.io dan java.util
 import java.io.*;
 import java.util.*;
 //Revisi: revisi line 40
 public class Main {
+    // Inisiasi inputReader dan printWriter
     private static InputReader in = new InputReader(System.in);
     private static PrintWriter out = new PrintWriter(System.out);
 
+    // Deklarasi variabel daftarMobil untuk menyimpan daftar mobil
     static Mobil[] daftarMobil;
   
-    public static void main(String[] args) {      
+    // Method main sebagai lokasi utama berjalannya program
+    public static void main(String[] args) {    
+        // Mengambil jumlah mobil yang ingin digunakan
         int P = in.nextInt();
         daftarMobil = new Mobil[P];
+
+        // Mengambil data mobil oleh user
         for(int i = 0; i < P; i++){
             String nama = in.next();
             String jenis = in.next();    // dijamin salah satu dari Terbang, Air, Truk
             String behaviour = in.next();  // dijamin salah satu dari Listrik atau BBM
             String bahanBakar = in.next();
 
+            // Mengisi jenis dan behavior mesin dari mobil yang bersangkutan serta jenis bahan bakarnya
+            // Kemudian menginisiasi object sesuai dengan ketiga hal tersebut dan ditambahkan ke daftarMobil
             EngineBehaviour engineBehaviour = null;
             if(behaviour.equals("BBM")) engineBehaviour = new BBM();
             else if(behaviour.equals("Listrik")) engineBehaviour = new Listrik();
@@ -29,11 +38,14 @@ public class Main {
             }
         }
         
+        // Mengambil jumlah command yang ingin dilakukan
         int Q = in.nextInt();
+        // Mengambil setiap command oleh user
         for(int i = 0; i < Q; i++) {
             String nama = in.next();
             String perintah = in.next();
 
+            // Saat mobil tidak ada 
             Mobil mobilNow = null;
             // Mobil dijamin ada, tidak perlu handle kasus null
             for(Mobil m : daftarMobil) {
@@ -43,6 +55,14 @@ public class Main {
                 }
             }
             
+            // Penjalanan perintah ada beberapa hal
+            // 1) START -> memulai penghidupan mesin
+            // 2) GAS -> menjalankan mesin
+            // 3) STOP -> mematikan mesin
+            // 4) ISI -> mengisi bahan bakar mesin
+            // 5) SIMULASI -> mensimulasikan jalannya mesin
+            // Setelah command didapati program maka akan dicetak
+            // berdasarkan return yang ada di dalam object methodnya
             if (perintah.equals("START")){
                 out.println(mobilNow.start());
             }
@@ -63,6 +83,7 @@ public class Main {
             }
         }
 
+        // Menutup Scanner
         out.close();
     }
 
