0178ef1d6cae8ecc00c61d49d441b6874ba8ebe4
eugeniusms <eugeniusms@gmail.com>
2022-04-27 16:01:38 +0700

    Succes Visualize Profile Section

--- GUI-Challenge/DekDepeTalk.java
+++ GUI-Challenge/DekDepeTalk.java
@@ -5,6 +5,8 @@ import javax.swing.border.TitledBorder;
 import javax.swing.border.Border;
 
 public class DekDepeTalk extends JFrame {
+  // Import images
+  private ImageIcon profileIcon = new ImageIcon("images/dekdepe-profile.png");
   // Create text fields for interest rate, years
   // loan amount, monthly payment, and total payment
   private JTextField jtfTypeChat = new JTextField();
@@ -14,17 +16,37 @@ public class DekDepeTalk extends JFrame {
   private JTextField jtfMonthlyPayment = new JTextField();
   private JTextField jtfTotalPayment = new JTextField();
 
+  Color profileColor = new Color(50, 50, 50);
+
   // Create a Compute Payment button
   private JButton jbtComputeLoan = new JButton("Visualize");
   private JButton jbtSendMessage = new JButton("Kirim");
 
+  // Online Label
+  private JLabel profileName = new JLabel("Dek Depe");
+  private JLabel logActivity = new JLabel("Online");
+
   public DekDepeTalk() {
     // Panel p1 to hold labels and text fields
-    JPanel p1 = new JPanel(new GridLayout(3,1));
-    String spaces = "                   ";
-    p1.add(new JLabel(spaces + "            " + spaces));  
-    p1.add(new JLabel(spaces + "Dek Depe ^_^" + spaces));  
-    p1.add(new JLabel(spaces + "            " + spaces));   
+    JPanel profile = new JPanel(new FlowLayout());
+    profile.add(new JLabel(profileIcon));
+
+    JPanel seen = new JPanel(new GridLayout(2,1));
+    profileName.setForeground(Color.WHITE);
+    profileName.setFont(profileName.getFont().deriveFont(Font.BOLD, 14f));
+    seen.add(profileName);
+    logActivity.setForeground(Color.GREEN);
+    seen.add(logActivity);
+    seen.setBackground(profileColor);
+
+    profile.add(seen);
+    profile.setBackground(profileColor);
+
+    JPanel p1 = new JPanel(new GridLayout(1,2));
+    p1.add(profile);
+    p1.add(new JLabel());
+    p1.setBackground(profileColor);
+
     //Create a border
     Border blackline = BorderFactory.createLineBorder(Color.black);
     p1.setBorder(blackline);
@@ -32,6 +54,7 @@ public class DekDepeTalk extends JFrame {
     // Panel p2 to hold the button
     JPanel p2 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
     p2.add(jbtComputeLoan);
+    p2.setBorder(blackline);
 
     // Panel p3 untuk memvisualisasikan grafik
     JPanel p3 = new JPanel(new GridLayout(3,1));
